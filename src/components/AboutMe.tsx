500928d4e11b83ff742e1b13a84786ed33be47b7
fabell-dev <ojeafabian66@gmail.com>
2025-10-20 20:43:15 -0700

    test

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -12,43 +12,44 @@ function AboutMe() {
     { category: "Frontend", name: "Css", img: css },
     { category: "Frontend", name: "#javascript", img: js },
     { category: "Frontend", name: "#Typescript", img: typesrcypt },
+    
 
     { category: "Frontend", name: "TailwindCSS", img: tailwind },
     { category: "Frontend", name: "react", img: react },
     { category: "Frontend", name: "Next.JS", img: next },
+    
   ];
   return (
     <>
       <section
         id="about"
-        className="cursor-default text-neutral-content py-30 px-4
+        className="cursor-default text-neutral-content md:py-30 py-20 px-4
         flex flex-col md:flex-row
-        items-center place-content-center mt-10 md:gap-10 sm:gap:5 gap-y-2"
+        items-center place-content-center md:mt-10 md:gap-5"
       >
-        <div className="md:w-100 md:h-90 w-80 h-65 relative bg-info-content/30 rounded-2xl text-balance text-left pl-10 md:shadow-[-5px_5px_5px_rgba(0,0,0,0.25)] flex">
-          <p className="md:text-5xl md:top-10 top-5 text-4xl font-light absolute  left-10">
+        <div className="md:w-100 md:h-90 w-80 h-50 bg-info-content/30 rounded-2xl text-balance text-center md:shadow-[-5px_5px_5px_rgba(0,0,0,0.25)] flex flex-col justify-center  items-center md:gap-y-10 gap-y-5">
+          <p className="md:text-5xl text-4xl font-light">
             about me
           </p>
-          <p className="md:text-sm md:top-30 text-xs absolute top-20 ">
+          <p className="md:text-[16px] md:mx-5 text-xs mx-2">
             I'm a 23 years old and junior front-end developer. I just finished
             my studies in computer engineering and am specializing in web
             development. During my studies, I participated in several web
             development projects and continue to study to become a true
             professional.
           </p>
-          <p className="md:text-sm md:bottom-15 text-xs absolute bottom-5">
+          <p className="hidden md:block md:text-[16px] md:mx-5 text-xs mx-2">
             With a constant thirst for learning and a commitment to detail, I'm
             ready for my first professional opportunity.
           </p>
         </div>
 
-        <div className="w-100 h-90 flex flex-col box-border">
-          <p className="text-5xl font-light self-start flex-1 mt-10">
+        <div className="md:w-100 md:h-90 w-80 h-60  flex flex-col box-border justify-between md:justify-normal md:gap-y-20">
+          <p className="md:text-5xl md:mt-5 text-4xl font-light  mt-10">
             my skills
           </p>
           <div
-            className=" w-100 flex-2 
-          grid grid-cols-4 grid-rows-2 justify-items-center"
+            className="w-80 md:w-100 grid grid-cols-4 grid-rows-2 justify-items-center gap-y-5 md:gap-y-10"
           >
             {skills.map((item) => (
               <img className="w-15 h-15 object-contain" src={item.img}></img>
