5e13bc764f52e85c5c764b09b51c0aeaecb677f4
fabell-dev <ojeafabian66@gmail.com>
2025-11-05 16:24:49 -0800

    probando

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -9,9 +9,10 @@ import next from "../assets/icons/nextjs.svg";
 
 import { useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
+import ClassNames from 'embla-carousel-class-names'
 
 function Proyects() {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true },[ClassNames()]);
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev();
@@ -67,8 +68,8 @@ function Proyects() {
         </div>
 
         {/* Moviles */}
-        <div className="flex self-center gap-5 md:hidden">
-          <div className="embla w-50 order-2">
+        <div className="flex self-center md:hidden">
+          <div className="embla w-80 order-2 ">
             <div className="embla__viewport" ref={emblaRef}>
               <div className="embla__container">
                 <Card
@@ -99,10 +100,10 @@ function Proyects() {
             </div>
           </div>
           <button
-            className="embla__prev w-10 h-10 order-1 self-center"
+            className="embla__prev w-10 h-10 order-1 self-center cursor-pointer translate-x-10 z-10"
             onClick={scrollPrev}
           >
-            <svg className="" viewBox="0 0 532 532">
+            <svg className="embla__button__svg w-10" viewBox="0 0 532 532">
               <path
                 fill="currentColor"
                 d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
@@ -110,10 +111,10 @@ function Proyects() {
             </svg>
           </button>
           <button
-            className="embla__next w-10 h-10 order-3 self-center"
+            className="embla__next w-10 h-10 order-3 self-center cursor-pointer -translate-x-10 z-10"
             onClick={scrollNext}
           >
-            <svg className="embla__button__svg" viewBox="0 0 532 532">
+            <svg className="embla__button__svg w-10" viewBox="0 0 532 532">
               <path
                 fill="currentColor"
                 d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
@@ -131,7 +132,7 @@ export default Proyects;
 export function Card(props: CardProps) {
   return (
     <div
-      className={`${props.class} flex flex-col border-neutral-content/80 border-1 w-50 rounded-xl shadow-[-5px_5px_5px_rgba(0,0,0,0.25)]`}
+      className={`${props.class} mx-10 my-5 flex flex-col border-neutral-content/50 border-2 w-80 rounded-xl shadow-[-5px_5px_5px_rgba(0,0,0,0.25)]`}
     >
       <p className="absolute self-center text-white cursor-default">
         {props.text}
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,44 +1,36 @@
-import { useCallback } from "react";
-import useEmblaCarousel from "embla-carousel-react";
+type Props = {};
 
-export const Test = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel();
-
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev();
-  }, [emblaApi]);
-
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext();
-  }, [emblaApi]);
-
+function Test({}: Props) {
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          <div className="embla__slide">Slide 1</div>
-          <div className="embla__slide">Slide 2</div>
-          <div className="embla__slide">Slide 3</div>
-        </div>
-      </div>
-      <button className="embla__prev w-10 h-10" onClick={scrollPrev}>
-        <svg className="" viewBox="0 0 532 532">
-          <path
-            fill="currentColor"
-            d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z"
-          />
-        </svg>
-      </button>
-      <button className="embla__next w-10 h-10" onClick={scrollNext}>
-        <svg className="embla__button__svg" viewBox="0 0 532 532">
-          <path
-            fill="currentColor"
-            d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
-          />
-        </svg>
-      </button>
-    </div>
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga
+      laboriosam minus totam modi accusantium dolorem voluptate repellendus
+      inventore molestias hic obcaecati, ut quisquam eaque harum impedit nam
+      placeat? Saepe. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Et, mollitia. Doloribus eius quia totam dolorum molestiae dolorem
+      consequuntur exercitationem dolore magnam aut repellendus quisquam,
+      repellat, corrupti error, ea veniam rerum! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Atque voluptatum in perspiciatis minus
+      quibusdam perferendis natus doloribus nostrum quisquam est cumque
+      explicabo quod, qui maiores aspernatur repudiandae neque dolorem
+      provident. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Inventore, ipsum officiis? Eos doloremque eaque laboriosam officiis et
+      quasi. Reiciendis illum quisquam ratione non eligendi cum eius ut sapiente
+      amet tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Officiis impedit quasi earum reiciendis consectetur beatae expedita
+      inventore. Nam, maxime tempora quisquam eum itaque voluptates ea fuga.
+      Possimus velit porro enim. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Eius consequatur natus, labore qui dolore fugit
+      necessitatibus nesciunt adipisci quisquam laborum earum, ipsum facilis
+      vitae enim incidunt maxime pariatur omnis. Temporibus. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Corrupti aperiam atque possimus.
+      Doloribus, soluta iure cupiditate commodi voluptatem dolore rem
+      asperiores, ratione hic voluptas odit veritatis architecto quia
+      temporibus! Libero. Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Voluptatibus odit ab reprehenderit, sint dolores recusandae iure
+      vitae commodi fugiat placeat natus rerum quia debitis magni hic
+      distinctio. Excepturi, ea nihil?
+    </>
   );
-};
-
+}
 export default Test;
